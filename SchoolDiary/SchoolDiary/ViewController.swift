41feff5381afe60b63376e2d8a10ad7188093d87
Annderson Pack <annderson.oreto@acad.pucrs.br>
2019-04-29 16:30:31 -0300

    First Commit

---
+++ SchoolDiary/SchoolDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SchoolDiary
+//
+//  Created by Annderson Packeiser Oreto on 29/04/19.
+//  Copyright © 2019 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
