270115fa5a1cfdc6dcfd9a9f889b0d7596f84211
Ronald Maciel <ronaldsmaciel2@outlook.com>
2019-04-30 19:18:30 -0300

    Add the basic skeleton of the TableView and a few structures of the cells

--- SchoolDiary/SchoolDiary/Controller/ViewController.swift
+++ SchoolDiary/SchoolDiary/Controller/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let sections = ["Alimentação", "Sono", "Evacuação", "Atividades", "Providenciar", "Fotos/ Vídeos"]
+    
+    let itemsOfSection = [
+        ["Lanche9h", "Almoço", "Lanche14h", "Pré-Janta", "Leite9h", "Leite17h30"],
+        ["manhã","tarde"],
+        ["Xixi","Cocô"],
+        ["Blocos de encaixe", "Balões"],
+        ["Pomada/Assadura" ]
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.sections[section]
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsOfSection[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "cell") as! UITableViewCell
+        cell.textLabel?.text = itemsOfSection[indexPath.section][indexPath.row]
+        
+        return cell
+    }
+    
+
 
 
 }
-
