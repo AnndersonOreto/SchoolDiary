3e77d43af5905af1ca038e2132d1180871e40157
Marcus Vinicius Vieira Badiale <marcus.badiale@gmail.com>
2019-04-30 15:22:37 -0300

    UiView Extension

---
+++ SchoolDiary/SchoolDiary/Extensions/UIView.swift
@@ -0,0 +1,24 @@
+//
+//  UIView.swift
+//  SchoolDiary
+//
+//  Created by Marcus Vinicius Vieira Badiale on 30/04/19.
+//  Copyright © 2019 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import UIKit
+
+extension UIView{
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
+
