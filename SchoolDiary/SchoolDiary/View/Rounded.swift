1649d511e1dde1cbddf1f37c99c6937e12f9778f
Annderson Pack <annderson.oreto@acad.pucrs.br>
2019-04-30 16:16:22 -0300

    Constraints Updated

---
+++ SchoolDiary/SchoolDiary/View/Rounded.swift
@@ -0,0 +1,21 @@
+//
+//  Rounded.swift
+//  SchoolDiary
+//
+//  Created by Annderson Packeiser Oreto on 30/04/19.
+//  Copyright © 2019 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import UIKit
+
+class Rounded: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
