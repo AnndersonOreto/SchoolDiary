821188b68926c68ebdf831b63e8ded558ba078ce
Annderson Pack <annderson.oreto@acad.pucrs.br>
2019-05-02 13:45:32 -0300

    LoginController

---
+++ SchoolDiary/SchoolDiary/Controller/LoginController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginController.swift
+//  SchoolDiary
+//
+//  Created by Annderson Packeiser Oreto on 02/05/19.
+//  Copyright © 2019 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
