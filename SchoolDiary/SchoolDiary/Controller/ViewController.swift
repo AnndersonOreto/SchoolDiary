a8092e6276c1e3deecb8d9c9ecc3becb4656bb8c
Annderson Pack <annderson.oreto@acad.pucrs.br>
2019-05-02 15:59:19 -0300

    Several Updates

--- SchoolDiary/SchoolDiary/Controller/ViewController.swift
+++ SchoolDiary/SchoolDiary/Controller/ViewController.swift
@@ -43,7 +43,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell =  tableView.dequeueReusableCell(withIdentifier: "cell") as! UITableViewCell
-        cell.textLabel?.text = itemsOfSection[indexPath.section][indexPath.row]
+        cell.textLabel?.text = itemsOfSection[indexPath.section][indexPath.row] 
         
         return cell
     }
