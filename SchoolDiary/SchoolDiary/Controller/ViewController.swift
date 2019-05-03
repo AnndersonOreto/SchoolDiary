7a243db432d09ee3d81a439b1c0888d7db8b530f
Marcus Vinicius Vieira Badiale <marcus.badiale@gmail.com>
2019-05-03 15:41:12 -0300

    change background and make cell constraints

--- SchoolDiary/SchoolDiary/Controller/ViewController.swift
+++ SchoolDiary/SchoolDiary/Controller/ViewController.swift
@@ -19,12 +19,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        tableView.delegate = self
         tableView.backgroundColor = #colorLiteral(red: 0.9254901961, green: 0.9254901961, blue: 0.9254901961, alpha: 1)
+        tableView.tableHeaderView?.backgroundColor = #colorLiteral(red: 0.9254901961, green: 0.9254901961, blue: 0.9254901961, alpha: 1)
     }
     
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sections[section]
-    }
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return sections[section]
+//    }
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return sections.count
@@ -33,11 +35,24 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: 30))
+
+        let headerImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 33, height: 30))
+        let image: UIImage = UIImage(named: "alimentacao1x")!
+        headerImageView.image = image
+        headerImageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.backgroundColor = #colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 1)
+        headerImageView.backgroundColor = #colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 1)
+        
+        view.addSubview(headerImageView)
+        
+        return view
+    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell =  tableView.dequeueReusableCell(withIdentifier: "cell") as! UITableViewCell
-//        cell.textLabel?.text = itemsOfSection[indexPath.section][indexPath.row]
-//        cell.textLabel?.text = "TEY"
         return cell
     }
     
