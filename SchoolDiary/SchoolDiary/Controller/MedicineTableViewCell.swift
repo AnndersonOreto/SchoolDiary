39f9004afa1ff231969bb33ff0f011822be3ab1d
Ronald Maciel <ronaldsmaciel2@outlook.com>
2019-05-08 18:25:49 -0300

    Creation of the classes for each cell and advance in the backbone; division of components of the health screen

---
+++ SchoolDiary/SchoolDiary/Controller/MedicineTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MedicineTableViewCell.swift
+//  SchoolDiary
+//
+//  Created by Ronald Maciel on 08/05/19.
+//  Copyright © 2019 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import UIKit
+
+class MedicineTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
