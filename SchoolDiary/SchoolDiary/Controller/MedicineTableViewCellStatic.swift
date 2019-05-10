32edd35b61deba5312745c18ee97d5eb9b4510d7
Ronald Maciel <ronaldsmaciel2@outlook.com>
2019-05-10 17:09:33 -0300

    Finished the sections

---
+++ SchoolDiary/SchoolDiary/Controller/MedicineTableViewCellStatic.swift
@@ -0,0 +1,24 @@
+//
+//  MedicineTableViewCellStatic.swift
+//  SchoolDiary
+//
+//  Created by Ronald Maciel on 10/05/19.
+//  Copyright © 2019 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import UIKit
+
+class MedicineTableViewCellStatic: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
