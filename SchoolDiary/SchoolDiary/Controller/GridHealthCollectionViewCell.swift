466c58213d5a9dc113d5c6371f74845f94ae2756
Ronald Maciel <ronaldsmaciel2@outlook.com>
2019-05-14 14:50:09 -0300

    Finished the configuration of the UIPicker and the title of toolbar for each measure

--- SchoolDiary/SchoolDiary/Controller/GridHealthCollectionViewCell.swift
+++ SchoolDiary/SchoolDiary/Controller/GridHealthCollectionViewCell.swift
@@ -13,9 +13,12 @@ class GridHealthCollectionViewCell: UICollectionViewCell {
     var picker = UIPickerView()
     var integerPart = Array(0...100)
     var decimalPart = [".0", ".1", ".2",".3",".4",".5",".6",".7",".8",".9"]
+
+    
     static var id = 0
     var id2 = 0
     
+
     @IBOutlet weak var measureImageView: UIImageView!
     @IBOutlet weak var measureLabel: UILabel!
     @IBOutlet weak var measureTextField: UITextField!
@@ -28,33 +31,31 @@ class GridHealthCollectionViewCell: UICollectionViewCell {
             measureTextField.inputView = picker
             self.createToolBar()
         }
+        
         id2 = GridHealthCollectionViewCell.id
         GridHealthCollectionViewCell.id += 1
     }
-    
+
+
+    // Func responsible for allow any change in the UIPicker
     @IBAction func textFieldEditingDidChange(_ sender: Any) {
-        if id2 < 2 {
-            measureTextField.text = measureTextField.text?.components(separatedBy: " ")[0]
-            if id2 == 0 {
-                measureTextField.text = "\(measureTextField.text ?? "") kg"
-            } else {
-                measureTextField.text = "\(measureTextField.text ?? "") cm"
-            }
-        }
+        
     }
     
 }
 
 extension GridHealthCollectionViewCell: UIPickerViewDelegate, UIPickerViewDataSource {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 2
+        return 4
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         if component == 0 {
             return integerPart.count
-        } else {
+        } else if component == 1 {
             return decimalPart.count
+        } else {
+            return 1
         }
         
     }
@@ -62,41 +63,81 @@ extension GridHealthCollectionViewCell: UIPickerViewDelegate, UIPickerViewDataSo
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         if component == 0 {
             return "\(integerPart[row])"
-        } else {
+        } else if component == 1 {
             return "\(decimalPart[row])"
+        } else if component == 3 {
+            if id2 == 0 {
+                return "kg"
+            } else {
+                return "cm"
+            }
+        } else {
+            return ""
         }
         
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         if measureTextField.isFirstResponder {
-            var intPart = ""
-            var decPart = ""
-            
-            if component == 0 {
-                intPart = "\(integerPart[row])"
+            if id2 == 0 {
+                measureTextField.text = "\(integerPart[pickerView.selectedRow(inComponent: 0)])\(decimalPart[pickerView.selectedRow(inComponent: 1)])kg"
+            } else {
+                measureTextField.text = "\(integerPart[pickerView.selectedRow(inComponent: 0)])\(decimalPart[pickerView.selectedRow(inComponent: 1)])cm"
             }
-            
-            if component == 1 {
-                decPart = decimalPart[row]
-            }
-            measureTextField.text = "\(intPart)\(decPart)"
         }
     }
     
     func createToolBar() {
+        picker.backgroundColor = #colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 1)
+        
+//        let screenSize: CGRect = UIScreen.main.bounds
+//        let view = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 30))
+//        var customView = GridHealthTableViewCell(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height))
+//
         if measureTextField.inputAccessoryView == nil {
             let toolBar = UIToolbar()
             toolBar.barStyle = .default
             toolBar.isTranslucent = true
             toolBar.sizeToFit()
+            toolBar.layer.borderWidth = 0.5
+            toolBar.layer.borderColor = #colorLiteral(red: 0.5921568627, green: 0.5921568627, blue: 0.5921568627, alpha: 1)
+            toolBar.clipsToBounds = true
+            
+            
             
             let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneClick))
             let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
             let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelClick))
-            toolBar.setItems([cancelButton, spaceButton, doneButton], animated: true)
+            
+            
+            
+            let titleForMeasureEditing = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+            titleForMeasureEditing.text = "Text in toolbar"
+            titleForMeasureEditing.textColor = #colorLiteral(red: 0.2156862745, green: 0.2156862745, blue: 0.2156862745, alpha: 1)
+            titleForMeasureEditing.font = UIFont(name: "System", size: 20)
+            titleForMeasureEditing.sizeToFit()
+            titleForMeasureEditing.textColor = .black
+            titleForMeasureEditing.textAlignment = NSTextAlignment.center
+            
+            
+            
+            if GridHealthCollectionViewCell.id == 0 {
+                // create the title for the weight measure
+                titleForMeasureEditing.text = "Editar peso"
+                let toolBarTitle = UIBarButtonItem(customView: titleForMeasureEditing)
+                toolBar.setItems([cancelButton, spaceButton,toolBarTitle, spaceButton, doneButton], animated: true)
+
+            } else {
+                // create the title for the height measure
+                titleForMeasureEditing.text = "Editar altura"
+                let toolBarTitle = UIBarButtonItem(customView: titleForMeasureEditing)
+                toolBar.setItems([cancelButton, spaceButton,toolBarTitle, spaceButton, doneButton], animated: true)
+            }
+            
             toolBar.isUserInteractionEnabled = true
+            toolBar.setNeedsLayout()
             measureTextField.inputAccessoryView = toolBar
+            
         }
     }
     
