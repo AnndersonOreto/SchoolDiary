93053129cb404be234d4a6bf1013ddc43b3140d5
Marcus Vinicius Vieira Badiale <marcus.badiale@gmail.com>
2019-05-14 14:20:37 -0300

    Assets profile's screen

---
+++ SchoolDiary/SchoolDiary/Controller/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  SchoolDiary
+//
+//  Created by Marcus Vinicius Vieira Badiale on 14/05/19.
+//  Copyright © 2019 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
