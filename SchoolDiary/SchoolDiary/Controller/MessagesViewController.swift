72ea345f9b3ec3d6a6fd0c54d433c6de0b797628
Vinícius Chagas <vcsoares@inf.ufrgs.br>
2019-05-14 15:58:04 -0300

    Setup chat list layout

--- SchoolDiary/SchoolDiary/Controller/MessagesViewController.swift
+++ SchoolDiary/SchoolDiary/Controller/MessagesViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class MessagesViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 76
     }
     
 
@@ -27,4 +32,27 @@ class MessagesViewController: UIViewController {
     }
     */
 
+}
+
+extension MessagesViewController: UITableViewDelegate {
+    
+}
+
+extension MessagesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath)
+        
+        cell.textLabel?.text = "Professora Ana"
+        cell.detailTextLabel?.text = "Ela está ótima. Não teve tosse hoje."
+        cell.imageView?.image = UIImage(named: "Ana")
+        cell.imageView?.cornerRadius = 22.5
+        
+        return cell
+    }
+    
+    
 }
