d017e84d1f0b545d8dbd229d68c63ab9664b569c
Marcus Vinicius Vieira Badiale <marcus.badiale@gmail.com>
2019-05-14 19:54:04 -0300

    Continuing invoice screen

---
+++ SchoolDiary/SchoolDiary/Controller/DetailInvoiceViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailInvoiceViewController.swift
+//  SchoolDiary
+//
+//  Created by Marcus Vinicius Vieira Badiale on 14/05/19.
+//  Copyright © 2019 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import UIKit
+
+class DetailInvoiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
